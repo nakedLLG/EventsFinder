8207df2eaf499c439830b5791391d06b64147df0
zanibellato <valerio.zanibellato@gmail.com>
2015-10-10 20:51:37 +0200

    Try to getting events from eventbrite

---
+++ EventsFinder/Scripts/eventbrite.js
@@ -0,0 +1,15 @@
+﻿jQuery(document).ready(function () {
+    Eventbrite({ 'app_key': "YPOIM2AUNBOTUEFT7H" }, function (eb) {
+        // NEW CODE
+        var eb_options = {
+            'user': 'email@address.abc',
+            'event_statuses': 'live, started'
+        };
+        eb.user_list_events(eb_options, function (response) {
+            // END NEW CODE
+            var eventbrite_list = eb.utils.eventList(response, eb.utils.eventListRow);
+            jQuery("#eventbrite-list").html(eventbrite_list);
+            console.log(response);
+        });
+    });
+});
