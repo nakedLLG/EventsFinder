112eaa72780c5b079b32b5d5c9e348ac5f6c7853
zanibellato <valerio.zanibellato@gmail.com>
2015-10-10 19:30:41 +0200

    maps done

---
+++ EventsFinder/Scripts/gmaps.js
@@ -0,0 +1,17 @@
+﻿function initMap() {
+    var myLatLng = { lat: 56.162, lng: 10.203 };
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: true,
+        zoom: 10
+    });
+
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: myLatLng,
+        title: 'We are here'
+    });
+}
